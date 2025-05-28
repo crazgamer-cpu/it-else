c4de8cef7fbde577068c734c97c991d1e4bf5c61
crazgamer-cpu <singhmeetpal453@gmail.com>
2025-05-28 16:47:08 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+nnum = int(input("Enter an integer: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+total = 0
+for i in range(1, 51):
+    total += i  
+print("The sum of integers from 1 to 50 is:", total)
+
